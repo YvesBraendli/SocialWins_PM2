ba35a1a55638a8320d17272a7917e25d05201898
moserna2 <moserna2@students.zhaw.ch>
2021-04-20 21:49:54 +0200

    add player class

---
+++ app/src/main/java/ch/zhaw/pm2/socialWins/Player.java
@@ -0,0 +1,9 @@
+package ch.zhaw.pm2.socialWins;
+
+import ch.zhaw.pm2.socialWins.strategy.Strategy;
+
+public class Player {
+	private String name;
+	private Strategy strategy;
+	// private Color color; Use own Enum for color?
+}
