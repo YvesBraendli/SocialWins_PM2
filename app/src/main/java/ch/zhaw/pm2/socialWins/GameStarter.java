21c4dbc04b4d8e4226c61e7b65799278e73db0f2
braenyve <braenyve@students.zhaw.ch>
2021-04-27 13:22:52 +0200

    created GameStarter

---
+++ app/src/main/java/ch/zhaw/pm2/socialWins/GameStarter.java
@@ -0,0 +1,20 @@
+package ch.zhaw.pm2.socialWins;
+
+import javafx.application.Application;
+/**
+ * This class starts the game application.
+ * @author Moser Nadine, Meier Robin, Braendli Yves
+ *
+ */
+public class GameStarter {
+	
+	/**
+	 * The main method for the game class.
+	 * 
+	 * @param args The application, which should be launched.
+	 */
+	public static void main([]String args) {
+		Application.launch(Game.class, args);
+	}
+
+}
