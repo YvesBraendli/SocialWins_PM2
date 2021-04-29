6bdf5c99c6df9fa1a5915cbcd82c36d5700e7ae2
meierr02 <meierr02@students.zhaw.ch>
2021-04-29 21:11:24 +0200

    Implemented switchinn between singleplayer options and multiplayer options

--- app/src/main/java/ch/zhaw/pm2/socialWins/setupWindowController.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/setupWindowController.java
@@ -2,11 +2,18 @@ package ch.zhaw.pm2.socialWins;
 
 
 
+import java.io.IOException;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
 
 public class setupWindowController {
 	@FXML private ChoiceBox<Integer> playerNumberChoiceBox;
@@ -16,6 +23,9 @@ public class setupWindowController {
 	@FXML private RadioButton computerLevelThreeRadioButton;
 	@FXML private ToggleGroup aiDifficulty;
 	@FXML private ChoiceBox<Integer> winningRowChoiceBox;
+	@FXML private Button startButton;
+	@FXML private BorderPane alternateView;
+		
 	private static final int DEFAULT_NUMBER_OF_PLAYERS = 1;
 	private static final int MAX_NUMBER_OF_PLAYERS = 8;
 	private static final int DEFAULT_WINNINGROW = 4;
@@ -32,7 +42,28 @@ public class setupWindowController {
 		}
 		playerNumberChoiceBox.setValue(DEFAULT_NUMBER_OF_PLAYERS);
 		winningRowChoiceBox.setValue(DEFAULT_WINNINGROW);
+		
+		playerNumberChoiceBox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+			public void changed(ObservableValue ov, Number value, Number new_value) {
+				try {
+					if(new_value.intValue() == 0) {
+						FXMLLoader loader = new FXMLLoader(getClass().getResource("setupViewA.fxml"));
+						loader.setController(this);
+						alternateView.setCenter(loader.load());
+					}
+					else {
+						FXMLLoader loader = new FXMLLoader(getClass().getResource("setupViewB.fxml"));
+						loader.setController(this);
+						alternateView.setCenter(loader.load());
+					}
+				}
+				catch(IOException e) {
+		            e.printStackTrace();
+				}
+			}
+		});
 	}
+
 
 	@FXML
 	private void startGame() {
