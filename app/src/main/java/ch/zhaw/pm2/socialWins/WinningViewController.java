aed9165de55137a70229ccdf8156d469fe9ad152
braenyve <braenyve@students.zhaw.ch>
2021-04-29 14:53:24 +0200

    added labels to gridPane and the method for changing the individual color of each label

--- app/src/main/java/ch/zhaw/pm2/socialWins/WinningViewController.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/WinningViewController.java
@@ -1,15 +1,31 @@
 package ch.zhaw.pm2.socialWins;
 
+import java.awt.Insets;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class WinningViewController {
 	private Game game;
 	private Parent parentSceneGraph;
+	private static final int GRID_PANE_HEIGHT = 380;
+	private static final int GRID_PANE_WIDTH = 380;
 	@FXML Button newGameButton;
 	@FXML Button closeButton;
 	@FXML GridPane showGameFieldGridPane;
@@ -21,7 +37,7 @@ public class WinningViewController {
 	 */
 	public void setUpWinningView(Game model) {
 		game = model;
-		showGameField();
+		showGameField(10,10);
 		showWinningText();
 	}
 	
@@ -45,8 +61,23 @@ public class WinningViewController {
 		stage.close();
 	}
 	
-	private void showGameField() {
-		
+	private void showGameField(int numberOfColumns, int numberOfRows) {
+		showGameFieldGridPane.setGridLinesVisible(true);
+		double gridElementHeight = GRID_PANE_HEIGHT/numberOfColumns;
+		double gridElementWidth = GRID_PANE_WIDTH/numberOfRows;
+		for (int i = 0; i<numberOfRows; i++) {
+			for(int z = 0; z<numberOfColumns; z++) {
+				Label newGridElement = new Label("");
+				newGridElement.setMaxSize(gridElementWidth, gridElementHeight);
+				newGridElement.setMinSize(gridElementWidth, gridElementHeight);
+				newGridElement.setBackground(new Background(new BackgroundFill(Color.PINK, new CornerRadii(0.0), new javafx.geometry.Insets(0.0)))); // Get Color from game
+				newGridElement.setBorder(new Border(
+	                    new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(1))));
+				GridPane.setRowIndex(newGridElement, i);
+				GridPane.setColumnIndex(newGridElement, z);
+				showGameFieldGridPane.getChildren().add(newGridElement);
+			}
+		}
 	}
 	
 	private void showWinningText() {
