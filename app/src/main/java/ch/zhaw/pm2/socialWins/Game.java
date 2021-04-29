96ba009ea767db5f82387e2559b414f78b4bb8a1
moserna2 <moserna2@students.zhaw.ch>
2021-04-29 22:51:46 +0200

    add structur of methods for game class acording to issue 08 implement game class

--- app/src/main/java/ch/zhaw/pm2/socialWins/Game.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/Game.java
@@ -1,16 +1,33 @@
 package ch.zhaw.pm2.socialWins;
 
+import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
-	private List<Player> players = new ArrayList<>();
+	private Player[] players;
 	private SocialWinsBoard board;
 	private int winningLineLength;
+	private Player winner;
 	
-	public Game(int winningLineLength) {
+	public Game(int winningLineLength, int NumberOfPlayers) {
+		// if winningLine is not in range throw exception
+		// if numberofplayer is not in range throw exception
+		
 		this.winningLineLength = winningLineLength;
+		players = new Player[NumberOfPlayers];
 		int rows = 10; // refactor
 		board = new SocialWinsBoard(rows);
+	}
+	
+	public void start() {
+		// have all players been set? otherwise stop.
+	}
+	
+	public void addPlayer(String name, Color color) {
+		
+	}
+	
+	public Player getWinner() {
 	}
 }
