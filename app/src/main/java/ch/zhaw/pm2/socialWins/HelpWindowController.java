76974b4adeda952bd9eaaadccb8b2979ed0ee983
braenyve <braenyve@students.zhaw.ch>
2021-05-01 12:20:06 +0200

    changed TextArea to Label in HelpView

--- app/src/main/java/ch/zhaw/pm2/socialWins/HelpWindowController.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/HelpWindowController.java
@@ -3,14 +3,21 @@ package ch.zhaw.pm2.socialWins;
 
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 
 public class HelpWindowController {
 	
 	private Game game; 
 	
-	@FXML TextArea showHelpTextArea;
+	@FXML Label showHelpLabel;
 
 	/**
 	 * Is called from GameUI to connect the Controller to the game model.
@@ -22,7 +29,9 @@ public class HelpWindowController {
 	}
 	
 	private void showHelp() {
-		showHelpTextArea.setText("Endlich funktioniert dieser Kack!"); // Add import of help text from game class.
+		showHelpLabel.setWrapText(true);
+		showHelpLabel.setBorder(new Border(new BorderStroke(Color.LIGHTGREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+		showHelpLabel.setText("Endlich funktioniert dieser Kack!"); // Add import of help text from game class.
 	}
 
 }
