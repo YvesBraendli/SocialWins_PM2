5b19b45c93c2c7ab2bbe1388cb58ce0c791f7ad9
meierr02 <meierr02@students.zhaw.ch>
2021-05-02 16:31:55 +0200

    implemented input validation

--- app/src/main/java/ch/zhaw/pm2/socialWins/SetupWindowController.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/SetupWindowController.java
@@ -2,8 +2,11 @@ package ch.zhaw.pm2.socialWins;
 
 
 
+import java.awt.Color;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -11,6 +14,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
@@ -24,6 +29,7 @@ public class SetupWindowController {
 	@FXML private ChoiceBox<Integer> winningRowChoiceBox;
 	@FXML private Button startButton;
 	@FXML private BorderPane alternateView;
+	@FXML private Label errorMessageLabel;
 
 	@FXML private TextField singlePlayerNameTextField;
 	@FXML private RadioButton computerLevelOneRadioButton;
@@ -34,13 +40,19 @@ public class SetupWindowController {
 	@FXML private ScrollPane playerNameScrollPane;
 	@FXML private VBox playerNameVBox;
 	
-	ArrayList<TextField> playerNames;
+	ArrayList<TextField> playerNamesTextFields;
+	ArrayList<ColorPicker> playerColorPickers;
 		
 	private static final int DEFAULT_NUMBER_OF_PLAYERS = 1;
 	private static final int MAX_NUMBER_OF_PLAYERS = 8;
 	private static final int DEFAULT_WINNINGROW = 4;
 	private static final int LOWEST_POSSIBLE_WINNINGROW = 3;
 	private static final int HIGHEST_POSSIBLE_WINNINGROW = 6;
+	private static final double BOARDSIZE_MULTIPLIKATOR = 1.5;
+	private static final String ALLOWED_PLAYERNAME_PATTERN = "^\\w+(-\\w+)*$";
+	
+	private double rowSize;
+	private double columnSize; 
 	
 	@FXML public void initialize() {
 		System.out.println("why does this get loaded so many times...pls help");
@@ -49,7 +61,7 @@ public class SetupWindowController {
 			for(int i = 1; i <= MAX_NUMBER_OF_PLAYERS; i++) {
 				playerNumberChoiceBox.getItems().add(i);
 			}
-			playerNumberChoiceBox.setValue(1);
+			playerNumberChoiceBox.setValue(DEFAULT_NUMBER_OF_PLAYERS);
 			
 			for(int i = LOWEST_POSSIBLE_WINNINGROW; i <= HIGHEST_POSSIBLE_WINNINGROW; i++) {
 				winningRowChoiceBox.getItems().add(i);
@@ -66,16 +78,24 @@ public class SetupWindowController {
 					loadSetupView('A');
 				} else {
 					loadSetupView('B');
-					playerNames  = new ArrayList<TextField>();
-					int scrollPaneSize = 0;
-					for (int i = 0; i <= new_value.intValue(); i++) {
-						playerNames.add(new TextField());
-						playerNames.get(i).setPromptText("Player " + (i + 1) + " name");
-						scrollPaneSize += 31;
-					}
-					playerNameVBox.getChildren().addAll(playerNames);
-					playerNameScrollAnchor.setPrefHeight(scrollPaneSize);
+					generatePlayerDataFields(new_value);
 				}
+			}
+
+			private void generatePlayerDataFields(Number new_value) {
+				playerNamesTextFields  = new ArrayList<TextField>();
+				playerColorPickers = new ArrayList<ColorPicker>();
+				int scrollPaneSize = 0;
+				for (int i = 0; i <= new_value.intValue(); i++) {
+					playerNamesTextFields.add(new TextField());
+					playerNamesTextFields.get(i).setPromptText("Player " + (i + 1) + " name");
+					playerNameVBox.getChildren().add(playerNamesTextFields.get(i));
+					playerColorPickers.add(new ColorPicker());
+					playerColorPickers.get(i).setMinHeight(24);
+					playerNameVBox.getChildren().add(playerColorPickers.get(i));
+					scrollPaneSize += 61;
+				}
+				playerNameScrollAnchor.setPrefHeight(scrollPaneSize);
 			}
 		});
 	}
@@ -91,9 +111,66 @@ public class SetupWindowController {
 		}
 	}
 
-
 	@FXML
 	private void startGame() {
+		int selectedwinningRowSize = winningRowChoiceBox.getSelectionModel().getSelectedItem();
+		int selectedplayerNumber = playerNumberChoiceBox.getSelectionModel().getSelectedItem();
+				
+		calculateBoardSize(selectedwinningRowSize, selectedplayerNumber);
 		
+		if(selectedplayerNumber > 1) {
+			startMultiplayerGame(selectedplayerNumber);
+		}
+		else {	
+			startSinglePlayerGame();
+		}
+	}
+
+	private void calculateBoardSize(int selectedwinningRowSize, int selectedplayerNumber) {
+		rowSize = BOARDSIZE_MULTIPLIKATOR * (selectedwinningRowSize-2+selectedplayerNumber);
+		if(!(rowSize%1==0)) {
+			rowSize=Math.ceil(rowSize);
+		}
+		columnSize=rowSize+1;
+	}
+
+	private void startSinglePlayerGame() {
+		String playerName = singlePlayerNameTextField.getText();
+		if(!playerName.matches(ALLOWED_PLAYERNAME_PATTERN)) {
+			errorMessageLabel.setText("Invalid name");
+			return;
+		}
+		
+		RadioButton selectedAiDifficulty = (RadioButton) aiDifficulty.getSelectedToggle();
+		int level = Integer.parseInt(selectedAiDifficulty.getText());
+		//Game game = new Game(selectedwinningRowSize, playerName, (int) columnSize, (int) rowSize);
+	}
+
+	private void startMultiplayerGame(int selectedplayerNumber) {
+		HashMap<Color, String> playerData = new HashMap<Color, String>();
+		for(int i = 0; i < selectedplayerNumber; i++) {
+			Color playerColor = new Color((float) playerColorPickers.get(i).getValue().getRed(),
+		            (float) playerColorPickers.get(i).getValue().getGreen(),
+		            (float) playerColorPickers.get(i).getValue().getBlue(),
+		            (float) playerColorPickers.get(i).getValue().getOpacity());
+			String playerName = playerNamesTextFields.get(i).getText();
+			
+			if(!playerName.matches(ALLOWED_PLAYERNAME_PATTERN)) {
+				errorMessageLabel.setText("Invalid names");
+				return;
+			}
+			for(Color color: playerData.keySet()) {
+				if(color.equals(playerColor)) {
+					errorMessageLabel.setText("Invalid colors");
+					return;
+				}
+				if(playerData.get(color).equals(playerName)) {
+					errorMessageLabel.setText("Invalid names");
+					return;
+				}
+			}
+			playerData.put(playerColor, playerName);
+		}
+		//Game game = new Game(selectedwinningRowSize, playerData, (int) columnSize, (int) rowSize);
 	}
 }
