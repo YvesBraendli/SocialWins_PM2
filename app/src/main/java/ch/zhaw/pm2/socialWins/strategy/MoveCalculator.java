28b96a00cb4865bafd8c5b8918232af31fe0a306
meierr02 <meierr02@students.zhaw.ch>
2021-05-04 19:18:58 +0200

    fixed buggy horizontal and vertical check

--- app/src/main/java/ch/zhaw/pm2/socialWins/strategy/MoveCalculator.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/strategy/MoveCalculator.java
@@ -18,37 +18,38 @@ public class MoveCalculator {
 	public Move calculateComputerMove(int depth, Board board, Boolean isMaximizing, int setColumn) {
 		numberOfColumns = board.getBoard()[0].length;
 		numberOfRows = board.getBoard().length;
-	
 		ArrayList<Integer> validColumns = getValidColumns(board);
 		
-		if(depth == 0) {
-				return new Move(setColumn, evaluateState(board.getBoard(), Config.SINGLEPLAYER_COMPUTERCOLOR));
-		}
 		
-		if(isMaximizing) {
-			Move move = new Move(0,-100000);
-			for(int column: validColumns) {
-				Board boardCopy = new Board(board.getBoard().clone());
-				boardCopy.addChip(column, Config.SINGLEPLAYER_COMPUTERCOLOR);
-				Move nextMove = calculateComputerMove(depth-1, boardCopy, false, column);
-				if(nextMove.getScore() > move.getScore()) {
-					move = nextMove;
-				}
-			}
-			return move;
-		}
-		else {
-			Move move = new Move(0,100000);
-			for(int column: validColumns) {
-				Board boardCopy = new Board(board.getBoard().clone());
-				boardCopy.addChip(column, Config.SINGLEPLAYER_USERCOLOR);
-				Move nextMove = calculateComputerMove(depth-1, boardCopy, true, column);
-				if(nextMove.getScore() < move.getScore()) {
-					move = nextMove;
-				}
-			}
-			return move;
-		}
+		board.addChip(setColumn, Config.SINGLEPLAYER_COMPUTERCOLOR);
+//		if(depth == 0) {
+				return new Move(setColumn, evaluateState(board.getBoard(), Config.SINGLEPLAYER_COMPUTERCOLOR));
+//		}
+		
+//		if(isMaximizing) {
+//			Move move = new Move(0,-100000);
+//			for(int column: validColumns) {
+//				Board boardCopy = new Board(board.getBoard().clone());
+//				boardCopy.addChip(column, Config.SINGLEPLAYER_COMPUTERCOLOR);
+//				Move nextMove = calculateComputerMove(depth-1, boardCopy, false, column);
+//				if(nextMove.getScore() > move.getScore()) {
+//					move = nextMove;
+//				}
+//			}
+//			return move;
+//		}
+//		else {
+//			Move move = new Move(0,100000);
+//			for(int column: validColumns) {
+//				Board boardCopy = new Board(board.getBoard().clone());
+//				boardCopy.addChip(column, Config.SINGLEPLAYER_USERCOLOR);
+//				Move nextMove = calculateComputerMove(depth-1, boardCopy, true, column);
+//				if(nextMove.getScore() < move.getScore()) {
+//					move = nextMove;
+//				}
+//			}
+//			return move;
+//		}
 	}
 	
 	private ArrayList<Integer> getValidColumns(Board board) {
@@ -91,16 +92,16 @@ public class MoveCalculator {
 		int score = 0;
 		for (int i = 0; i < numberOfRows - (Config.POINT_BLOCK_SIZE - 1); i++) {
 			for (int j = 0; j < numberOfColumns - (Config.POINT_BLOCK_SIZE - 1); j++) {
-
-				for (int k = 0; k < Config.POINT_BLOCK_SIZE; k++) {
-					Chip[] block = new Chip[Config.POINT_BLOCK_SIZE];
+				Chip[] block = new Chip[Config.POINT_BLOCK_SIZE];
+				for (int k = 0; k < Config.POINT_BLOCK_SIZE; k++) {	
 					block[k] = board[i - k + (Config.POINT_BLOCK_SIZE - 1)][j + k];
-					score += evaluateBlock(block, playedChipColor);
-
-					block = new Chip[Config.POINT_BLOCK_SIZE];
-					block[k] = board[i + k][j + k];
-					score += evaluateBlock(block, playedChipColor);
 				}
+				score += evaluateBlock(block, playedChipColor);
+				block = new Chip[Config.POINT_BLOCK_SIZE];
+				for (int k = 0; k < Config.POINT_BLOCK_SIZE; k++) {	
+					block[k] = board[i + k][j + k];
+				}
+				score += evaluateBlock(block, playedChipColor);
 			}
 		}
 		return score;
@@ -111,8 +112,8 @@ public class MoveCalculator {
 		for (int i = 0; i < numberOfColumns; i++) {
 			for (int j = 0; j < numberOfRows - (Config.POINT_BLOCK_SIZE - 1); j++) {
 				Chip[] block = new Chip[Config.POINT_BLOCK_SIZE];
-				for (int k = 0; k < Config.POINT_BLOCK_SIZE; k++) {
-					block[k] = board[j][i];
+				for (int k = j; k < Config.POINT_BLOCK_SIZE; k++) {
+					block[k-j] = board[k][i];
 				}
 				score += evaluateBlock(block, playedChipColor);
 			}
@@ -125,8 +126,8 @@ public class MoveCalculator {
 		for (int i = 0; i < numberOfRows; i++) {
 			for (int j = 0; j < numberOfColumns - (Config.POINT_BLOCK_SIZE - 1); j++) {
 				Chip[] block = new Chip[Config.POINT_BLOCK_SIZE];
-				for (int k = 0; k < Config.POINT_BLOCK_SIZE; k++) {
-					block[k] = board[i][j];
+				for (int k = j; k < Config.POINT_BLOCK_SIZE; k++) {
+					block[k-j] = board[i][k];
 				}
 				score += evaluateBlock(block, playedChipColor);
 			}
@@ -166,7 +167,7 @@ public class MoveCalculator {
 	private int checkForElementsInBlock(Chip[] block, Color chipColor) {
 		int numberOfElements = 0;
 		for (int i = 0; i < Config.POINT_BLOCK_SIZE; i++) {
-			if (block[i].getColor().equals(chipColor)) {
+			if (!(block[i] == null) && block[i].getColor().equals(chipColor)) {
 				numberOfElements += 1;
 			}
 		}
