f119ec4442756f9e45b70e3800d1ac89e22dd5c6
meierr02 <meierr02@students.zhaw.ch>
2021-05-06 09:48:59 +0200

    Added constructor

--- app/src/main/java/ch/zhaw/pm2/socialWins/strategy/ComputerIntermediateStrategy.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/strategy/ComputerIntermediateStrategy.java
@@ -1,6 +1,12 @@
 package ch.zhaw.pm2.socialWins.strategy;
 
 public class ComputerIntermediateStrategy implements Strategy{
+	
+	private MoveCalculator moveCalculator;
+	
+	public ComputerBeginnerStrategy(int numberOfRows, int numberOfColumns, int winningRowLength){
+		moveCalculator = new MoveCalculator(numberOfRows, numberOfColumns, winningRowLength);	
+	}
 
 	@Override
 	public void nextMove() {
