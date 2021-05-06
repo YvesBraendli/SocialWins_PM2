baf5a62545b55d73da65611c05ff59427fe329ec
meierr02 <meierr02@students.zhaw.ch>
2021-05-06 09:48:26 +0200

    Added Constructor

--- app/src/main/java/ch/zhaw/pm2/socialWins/strategy/ComputerBeginnerStrategy.java
+++ app/src/main/java/ch/zhaw/pm2/socialWins/strategy/ComputerBeginnerStrategy.java
@@ -2,6 +2,12 @@ package ch.zhaw.pm2.socialWins.strategy;
 
 public class ComputerBeginnerStrategy implements Strategy {
 	
+	private MoveCalculator moveCalculator;
+	
+	public ComputerBeginnerStrategy(int numberOfRows, int numberOfColumns, int winningRowLength){
+		moveCalculator = new MoveCalculator(numberOfRows, numberOfColumns, winningRowLength);	
+	}
+	
 	@Override
 	public void nextMove() {
 		throw new UnsupportedOperationException();
